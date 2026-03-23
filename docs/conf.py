ae1c1378e0802664e80f5e4cf1336b8fad8bb878
Gabriel Torres <gabrieltlt721@gmail.com>
2026-03-23 18:09:52 -0300

    :memo: changed readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,55 +1,38 @@
-project = 'Open_MICKY'
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Open MICKY'
 copyright = '2026, FBOT@Research'
 author = 'FBOT@Research'
 release = '0.1'
 
-extensions = ["myst_parser"]
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-source_suffix = {
-    ".md": "markdown"
-}
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.youtube'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
-    "table",
-]
 
-myst_heading_anchors = 3
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-html_css_files = [
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-]
-
-html_theme_options = {
-    # 🎨 cores
-    "light_css_variables": {
-        "color-brand-primary": "#2B5D9F",
-        "color-brand-content": "#2B5D9F",
-    },
-
-    # 🧭 navbar
-    "navbar_start": ["navbar-logo"],
-    "navbar_center": ["navbar-nav"],
-    "navbar_end": ["navbar-icon-links"],
-
-    # 🔗 ícones (GitHub)
-    "icon_links": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/fbot-research/open_micky",
-            "icon": "fab fa-github",
-        },
-    ],
-
-    # opcional (recomendado)
-    "navbar_align": "left",
-    "show_nav_level": 1,
-}
